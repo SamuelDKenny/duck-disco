b2f90b3a3ed8a9de37e4a977cfb3b6c6d6145fef
Samuel D Kenny <samuelkenny.email@gmail.com>
2026-01-04 19:36:49 +0000

    began work on method to move discs in TOH game

--- Towers Of Hanoi/Program.cs
+++ Towers Of Hanoi/Program.cs
@@ -1,22 +1,29 @@
-﻿    
-    
-    var t1 = new int[] { 1, 2, 3 };
-    var t2 = new int[] { 0, 0, 0 };
-    var t3 = new int[] { 0, 0, 0 };
+﻿
 
-    DisplayTowers(t1, t2, t3);
+var t1 = new int[] { 1, 2, 3 };
+var t2 = new int[] { 0, 0, 0 };
+var t3 = new int[] { 0, 0, 0 };
 
-    //win condition
-    //while(t3[0] == 0)
-   // {
-        //get user input
-        int inputFrom;
-        int inputTo;
-        Console.Write("\nEnter the tower number to move from (1, 2, or 3): ");
+DisplayTowers(t1, t2, t3);
+
+//win condition
+//while(t3[0] == 0)
+// {
+//get user input
+int inputFrom;
+int inputTo;
+Console.Write("\nEnter the tower number to move from (1, 2, or 3): ");
+string userChoice = Console.ReadLine();
+int.TryParse(userChoice, out inputFrom);
+
+//convert input to tower choice
 
 
-   // }
 
+// }
+
+
+//display towers method
 
 static void DisplayTowers(int[] t1, int[] t2, int[] t3)
 {
@@ -33,4 +40,32 @@ static void DisplayTowers(int[] t1, int[] t2, int[] t3)
         Console.WriteLine($"    {discs[t1[i]]} {discs[t2[i]]} {discs[t3[i]]}");
     }
     Console.WriteLine("  #####################################");
+}
+
+//check and move disc method
+
+static void TowerMove(int[] towerA, int[] towerB)
+{
+    //find moving value
+    bool validMove = true;
+    int moving = 0;
+    int iMoving = 0;
+
+    foreach (int value in towerA)
+    {
+        if (value > 0)
+        {
+            moving = value;
+            iMoving = Array.IndexOf(towerA, moving);
+            break;
+        }
+    }
+
+    if (moving == 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("There is nothing here to move!\n");
+    }
+
+    //find value of target top
 }
